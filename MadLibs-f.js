7786a4ac72f66d25eeb6bcda9bbc77c72ad568d4
moreinis <31930502+moreinis@users.noreply.github.com>
2017-10-17 19:36:23 -0400

    Tweak

--- MadLibs-f.js
+++ MadLibs-f.js
@@ -1,6 +1,19 @@
 var sentence = [];
 var index = 0;
 var replacements = [];
+//Input the sentence
+populate(sentence, "Word ",0);
+
+//Specify the replacements positions
+populate(replacements, "Position ",0);
+
+//Specify parts of speech
+swap(sentence,replacements,"Enter the PoS for ",0);
+
+//Get the replacements
+swap(sentence,replacements,"Enter a ",0);
+
+//
 sentence.splice(-1,1);
 sentenceString = sentence.join(' ');
 alert(sentenceString.toString());
@@ -21,15 +34,5 @@ for (index = 0; index < rarray.length - 1; index++){
 	return sarray;
 }
 
-//Input the sentence
-populate(sentence, "Word",0);
 
-//Specify the replacements positions
-populate(replacements, "Position",0);
-
-//Specify parts of speech
-swap(sentence,replacements,"Enter the PoS for ",0);
-
-//Get the replacements
-swap(sentence,replacements,"Enter a ",0);
 
